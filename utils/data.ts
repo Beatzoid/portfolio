d8e722f731992e07189956ef7126e52a932bf60b
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2022-01-03 10:57:37 -0800

    :fire: Remove Frontend Dev Skill

--- utils/data.ts
+++ utils/data.ts
@@ -6,11 +6,6 @@ import { IProject, IService } from "./types";
 
 export const services: IService[] = [
     {
-        title: "Frontend Development",
-        about: "I can build a beautiful and scalable frontend using <b>HTML</b>,<b>CSS</b> and <b>React.js</b> ",
-        Icon: RiComputerLine
-    },
-    {
         title: "Backend Development",
         about: "I can manage databases, servers, and apis using <b>Express</b>, <b>Fastify</b> & other popular frameworks",
         Icon: FaServer
